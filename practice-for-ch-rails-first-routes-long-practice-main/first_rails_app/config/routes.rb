cccc90a041bd62d14c4562f0ba73cfc9e092634c
swathi <swathibalasubramanyam@gmail.com>
2023-08-23 14:07:37 -0700

    Implemented phase 1

---
+++ practice-for-ch-rails-first-routes-long-practice-main/first_rails_app/config/routes.rb
@@ -0,0 +1,32 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  # resources :users
+#   this one line gives us below 8 urls
+#   Prefix Verb   URI Pattern               Controller#Action
+#     users GET    /users(.:format)          users#index
+#           POST   /users(.:format)          users#create
+#  new_user GET    /users/new(.:format)      users#new
+# edit_user GET    /users/:id/edit(.:format) users#edit
+#      user GET    /users/:id(.:format)      users#show
+#           PATCH  /users/:id(.:format)      users#update
+#           PUT    /users/:id(.:format)      users#update
+#           DELETE /users/:id(.:format)      users#destroy
+
+
+  get '/users', to: 'users#index'
+  post '/users', to: 'users#create'
+
+  get '/users/new', to: 'users#new'
+  get '/users/:id/edit', to: 'users#edit', as: 'edit_user' # returns a form to edit
+
+  get '/users/:id', to: 'users#show', as: 'user'
+  patch '/users/:id', to: 'users#update'
+
+  put '/users/:id', to: 'users#update'
+
+  delete '/users/:id', to: 'users#destroy'
+
+end
